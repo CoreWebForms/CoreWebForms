5c0877138ad484554c91aa530145bdc481038bc5
Taylor Southwick <tasou@microsoft.com>
2022-09-03 12:09:49 -0700

    Add initial logic

--- gen/UI.Generator/PageGenerator.cs
+++ gen/UI.Generator/PageGenerator.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.CodeAnalysis;
+using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Threading;
 
 namespace Microsoft.AspNetCore.SystemWebAdapters.UI.Generator;
 
@@ -7,5 +11,47 @@ public class PageGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
+        var aspxFiles = context.AdditionalTextsProvider
+            .Where(text => text.Path.EndsWith(".aspx", StringComparison.OrdinalIgnoreCase))
+            .Select((text, token) => Parse(text, token));
+        context.RegisterSourceOutput(aspxFiles, GenerateSource);
+    }
+
+    private void GenerateSource(SourceProductionContext context, ParsedPage page)
+    {
+        context.AddSource(page.Path, GetSource(page));
+    }
+
+    private string GetSource(ParsedPage page)
+    {
+        using var stringWriter = new StringWriter();
+
+        using (var writer = new IndentedTextWriter(stringWriter))
+        {
+            writer.Write("public partial class ");
+            writer.WriteLine(page.Name);
+            writer.WriteLine("{");
+            writer.WriteLine("}");
+        }
+
+        return stringWriter.ToString();
+    }
+
+    private ParsedPage Parse(AdditionalText text, CancellationToken token)
+    {
+        var path = text.Path.TrimStart('/') + ".g.cs";
+
+        return new ParsedPage
+        {
+            Path = path,
+            Name = "About",
+        };
+    }
+
+    private class ParsedPage
+    {
+        public string Path { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
     }
 }
--- test/Generator.Tests/PageGeneratorTests.cs
+++ test/Generator.Tests/PageGeneratorTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.SystemWebAdapters.UI.Generator;
 using Xunit;
 
 using VerifyCS = Microsoft.AspNetCore.SystemWebAdapters.UI.Generator.Tests.GeneratorVerifier<
@@ -11,5 +12,30 @@ public class PageGeneratorTests
     public async Task Empty()
     {
         await new VerifyCS.Test().RunAsync();
+    }
+
+    [Fact]
+    public async Task EmptyAspx()
+    {
+        var aspx = "<%@ Page Title=\"About\" Language=\"C#\" MasterPageFile=\"~/Site.Master\" AutoEventWireup=\"true\" CodeBehind=\"About.aspx.cs\" Inherits=\"WebApplication12.About\" %>\r\n";
+        var generated = @"public partial class About
+{
+}
+";
+
+        await new VerifyCS.Test()
+        {
+            TestState =
+            {
+                AdditionalFiles =
+                {
+                    ("/page.aspx", aspx),
+                },
+                GeneratedSources =
+                {
+                    (typeof(PageGenerator), "page.aspx.g.cs", generated),
+                },
+            },
+        }.RunAsync();
     }
 }
