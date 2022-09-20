97260a3c64e58281cd08b817422a270f1ef061c6
Taylor Southwick <tasou@microsoft.com>
2022-09-20 14:45:30 -0700

    Add a debugger.break call

--- samples/SystemWebUISample/Pages/MyPage.cs
+++ samples/SystemWebUISample/Pages/MyPage.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
 using System.Web.UI;
 
 namespace SystemWebUISample.Pages;
@@ -9,6 +10,6 @@ public class MyPage : Page
 {
     protected void Button1_Click(object sender, EventArgs e)
     {
-        // perform action  
+        Debugger.Break();
     }
 }
