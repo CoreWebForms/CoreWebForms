c3886fdd4fc66bae2a39c3bf8b35cb218eeb44ac
Taylor Southwick <tasou@microsoft.com>
2022-09-22 10:34:23 -0700

    Add htmllink

---
+++ src/WebForms/UI/HtmlControls/HtmlLink.cs
@@ -0,0 +1,56 @@
+//------------------------------------------------------------------------------
+// <copyright file="HtmlLink.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.ComponentModel;
+
+#nullable disable
+
+namespace System.Web.UI.HtmlControls;
+public class HtmlLink : HtmlControl
+{
+
+    public HtmlLink() : base("link")
+    {
+    }
+
+    [
+    WebCategory("Action"),
+    DefaultValue(""),
+    DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden),
+    UrlProperty(),
+    ]
+    public virtual string Href
+    {
+        get
+        {
+            string s = Attributes["href"];
+            return ((s != null) ? s : String.Empty);
+        }
+        set
+        {
+            Attributes["href"] = MapStringAttributeToString(value);
+        }
+    }
+
+
+    protected override void RenderAttributes(HtmlTextWriter writer)
+    {
+        // Resolve the client href based before rendering the attribute.
+        if (!String.IsNullOrEmpty(Href))
+        {
+            Attributes["href"] = ResolveClientUrl(Href);
+        }
+
+        base.RenderAttributes(writer);
+    }
+
+    protected internal override void Render(HtmlTextWriter writer)
+    {
+        writer.WriteBeginTag(TagName);
+        RenderAttributes(writer);
+        writer.Write(HtmlTextWriter.SelfClosingTagEnd);
+    }
+}
