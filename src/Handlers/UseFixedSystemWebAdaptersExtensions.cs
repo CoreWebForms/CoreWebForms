46568d63df82f7b0d0f248590875867d52fe3940
Taylor Southwick <tasou@microsoft.com>
2022-10-07 19:14:53 -0700

    add fixed feature

---
+++ src/Handlers/UseFixedSystemWebAdaptersExtensions.cs
@@ -0,0 +1,34 @@
+// MIT License.
+
+using System.IO.Pipelines;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.SystemWebAdapters;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public class UseFixedSystemWebAdaptersExtensions
+{
+    public void UseFixedSystemWebAdapters(IApplicationBuilder app)
+    {
+        app.UseSystemWebAdapters();
+        app.Use((ctx, next) =>
+        {
+            ctx.Features.Set<IRequestBodyPipeFeature>(new NewPipe(ctx.Features.GetRequired<IHttpRequestFeature>()));
+            return next(ctx);
+        });
+    }
+
+    private class NewPipe : IRequestBodyPipeFeature
+    {
+        private readonly IHttpRequestFeature _feature;
+        private PipeReader? _pipeReader;
+
+        public NewPipe(IHttpRequestFeature feature)
+        {
+            _feature = feature;
+        }
+
+        PipeReader IRequestBodyPipeFeature.Reader => _pipeReader ??= PipeReader.Create(_feature.Body, new StreamPipeReaderOptions(leaveOpen: true));
+    }
+}
