1b425787df64515acb9ea5d9f3dea8f8c0b34074
Taylor Southwick <tasou@microsoft.com>
2022-11-04 19:55:30 -0700

    Add extension

---
+++ src/Optimization/OptimizationExtensions.cs
@@ -0,0 +1,17 @@
+﻿// MIT License.
+
+using Microsoft.AspNetCore.SystemWebAdapters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace System.Web.Optimization;
+
+public static class OptimizationExtensions
+{
+    public static ISystemWebAdapterBuilder AddOptimization(this ISystemWebAdapterBuilder builder, Action<BundleReferenceOptions> configure)
+    {
+        builder.Services.AddOptions<BundleReferenceOptions>()
+            .Configure(configure);
+
+        return builder;
+    }
+}
