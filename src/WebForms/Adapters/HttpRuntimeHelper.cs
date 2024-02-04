a6de95a2526021d4033ad8880ac8abb28ea1567f
Taylor Southwick <tasou@microsoft.com>
2024-02-04 10:30:07 -0800

    Use HttpRuntime to serve RouteTable.Routes and BundleConfig.Bundles (#119)

--- src/WebForms/Adapters/HttpRuntimeHelper.cs
+++ src/WebForms/Adapters/HttpRuntimeHelper.cs
@@ -10,13 +10,24 @@ internal static class HttpRuntimeHelper
     private static readonly Lazy<Func<IServiceProvider>> ServiceProviderFactory = new(() =>
     {
         var type = typeof(HttpRuntime).Assembly.GetType("System.Web.Hosting.HostingEnvironmentAccessor");
+
+        if (type is null)
+        {
+            throw new InvalidOperationException("Could not find accessor");
+        }
+
         var currentProperty = type.GetProperty("Current", BindingFlags.Static | BindingFlags.Public);
         var spProperty = type.GetProperty("Services", BindingFlags.Instance | BindingFlags.Public);
+
+        if (currentProperty is null || spProperty is null)
+        {
+            throw new InvalidOperationException("Couldn't find HttpRuntime services");
+        }
 
         return () =>
         {
             var current = currentProperty.GetValue(null);
-            return (IServiceProvider)spProperty.GetValue(current);
+            return (IServiceProvider)spProperty.GetValue(current)!;
         };
     }, isThreadSafe: true);
 
