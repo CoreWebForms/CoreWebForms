5b525e9514d115de4db9c86d7d4522b32af09e3e
Taylor Southwick <tasou@microsoft.com>
2024-02-08 09:15:54 -0800

    add back

--- src/Extensions/Script/js/rollup.config.mjs
+++ src/Extensions/Script/js/rollup.config.mjs
@@ -1,24 +1,24 @@
 import preprocess from './preprocess.js'
 
 export default [
-  // {
-  //   input: [
-  //     "MicrosoftAjaxCore.jsa",
-  //     "MicrosoftAjaxWebForms.jsa",
-  //     "MicrosoftAjaxComponentModel.jsa",
-  //     "MicrosoftAjaxGlobalization.jsa",
-  //     "MicrosoftAjaxHistory.jsa",
-  //     "MicrosoftAjaxNetwork.jsa",
-  //     "MicrosoftAjaxSerialization.jsa",
-  //   ],
-  //   output: {
-  //     dir: "dist/",
-  //     format: "es",
-  //   },
-  //   plugins: [
-  //     preprocess()
-  //   ]
-  // },
+  {
+    input: [
+      "MicrosoftAjaxCore.jsa",
+      "MicrosoftAjaxWebForms.jsa",
+      "MicrosoftAjaxComponentModel.jsa",
+      "MicrosoftAjaxGlobalization.jsa",
+      "MicrosoftAjaxHistory.jsa",
+      "MicrosoftAjaxNetwork.jsa",
+      "MicrosoftAjaxSerialization.jsa",
+    ],
+    output: {
+      dir: "dist/",
+      format: "es",
+    },
+    plugins: [
+      preprocess()
+    ]
+  },
   {
     input: ["MicrosoftAjax.jsa"],
     output: {
