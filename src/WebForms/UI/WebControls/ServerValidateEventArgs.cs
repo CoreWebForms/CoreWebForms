fcf1a4005041eb3907d1c962bea0003ee734d3ef
Mason Chase <mason.chase@quandago.com>
2024-02-16 13:09:07 +0100

    Add ServerValidateEventArgs and WizardNavigationEventArgs

---
+++ src/WebForms/UI/WebControls/ServerValidateEventArgs.cs
@@ -0,0 +1,45 @@
+﻿// MIT License.
+
+namespace System.Web.UI.WebControls;
+
+using System;
+
+/// <devdoc>
+///    <para>Provides data for the
+///    <see langword='ServerValidate'/> event of the <see cref='System.Web.UI.WebControls.CustomValidator'/> .</para>
+/// </devdoc>
+public class ServerValidateEventArgs : EventArgs
+{
+    private bool isValid;
+    private string value;
+
+
+    /// <devdoc>
+    /// <para>Initializes a new instance of the <see cref='System.Web.UI.WebControls.ServerValidateEventArgs'/>
+    /// class.</para>
+    /// </devdoc>
+    public ServerValidateEventArgs(string value, bool isValid) : base()
+    {
+        this.isValid = isValid;
+        this.value = value;
+    }
+
+
+    /// <devdoc>
+    /// <para>Gets the string value to validate.</para>
+    /// </devdoc>
+    public string Value
+    {
+        get { return value; }
+    }
+
+
+    /// <devdoc>
+    ///    Gets or sets whether the input is valid.
+    /// </devdoc>
+    public bool IsValid
+    {
+        get { return isValid; }
+        set { this.isValid = value; }
+    }
+}
---
+++ src/WebForms/UI/WebControls/WizardNavigationEventArgs.cs
@@ -0,0 +1,39 @@
+﻿namespace System.Web.UI.WebControls;
+
+public class WizardNavigationEventArgs : EventArgs
+{
+  private int _currentStepIndex;
+  private int _nextStepIndex;
+  private bool _cancel;
+
+
+  public WizardNavigationEventArgs(int currentStepIndex, int nextStepIndex)
+  {
+    _currentStepIndex = currentStepIndex;
+    _nextStepIndex = nextStepIndex;
+  }
+
+
+  public bool Cancel
+  {
+    get { return _cancel; }
+    set { _cancel = value; }
+  }
+
+
+  public int CurrentStepIndex
+  {
+    get { return _currentStepIndex; }
+  }
+
+
+  public int NextStepIndex
+  {
+    get { return _nextStepIndex; }
+  }
+
+  internal void SetNextStepIndex(int nextStepIndex)
+  {
+    _nextStepIndex = nextStepIndex;
+  }
+}
