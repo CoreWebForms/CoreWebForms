f4326379f58d7755e8008214d9c89118708c885d
Mason Chase <mason.chase@quandago.com>
2024-03-08 10:16:51 +0100

    Fix typo

--- src/Profile/HttpContextProfileExtensions.cs
+++ src/Profile/HttpContextProfileExtensions.cs
@@ -5,7 +5,7 @@ using System.Web.Profile;
 namespace System.Web;
 
 // TODO: Implement
-public static class HttpContextExtensions
+public static class HttpContextProfileExtensions
 {
     public static ProfileInfo GetProfile(this HttpContext context)
     {
