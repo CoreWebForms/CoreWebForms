722458a1409d3847a658434a5c2b24a5be4f8520
Taylor Southwick <tasou@microsoft.com>
2024-03-28 13:44:15 -0700

    Decouple optimization from extensions (#273)

---
+++ src/Extensions/IBundleResolver.cs
@@ -0,0 +1,30 @@
+// MIT License.
+
+namespace WebForms.Extensions.Adapters;
+
+public interface IBundleResolver
+{
+    /// <summary>
+    /// Determines whether a virtual path addresses a bundle.
+    /// </summary>
+    /// <param name="virtualPath">The requested virtual path.</param>
+    /// <returns>True if the virtual path addresses a bundle.</returns>
+    bool IsBundleVirtualPath(string virtualPath);
+
+    /// <summary>
+    /// Gets the paths to files included in a bundle. 
+    /// </summary>
+    /// <param name="virtualPath">The requested virtual path.</param>
+    /// <returns>A list of all the virtual paths for the files contained in the bundle.</returns>
+    IEnumerable<string> GetBundleContents(string virtualPath);
+
+    /// <summary>
+    /// Gets a versioned bundle url.
+    /// </summary>
+    /// <param name="virtualPath">The requested virtual path.</param>
+    /// <returns>The versioned url for a bundle.</returns>
+    /// <remarks>
+    /// As an exmple, GetBundleUrl("~/bundle") will, assuming that ~/bundle addresses a valid bundle, yield the path string "~/bundle?v=149872359237"
+    /// </remarks>
+    string GetBundleUrl(string virtualPath);
+}
