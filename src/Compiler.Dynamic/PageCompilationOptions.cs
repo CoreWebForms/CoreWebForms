7eaf8d47e7ffac913c510a04a575460d3c99f412
Taylor Southwick <tasou@microsoft.com>
2024-03-28 15:26:57 -0700

    Add infra project and tag prefix option (#274)

--- src/Compiler.Dynamic/PageCompilationOptions.cs
+++ src/Compiler.Dynamic/PageCompilationOptions.cs
@@ -15,9 +15,13 @@ public class PageCompilationOptions
 
     public bool IsDebug { get; set; }
 
+    internal List<TagNamespaceRegisterEntry> Entries { get; } = [];
+
     internal IFileProvider WebFormsFileProvider { get; set; } = default!;
 
     internal Dictionary<string, Func<VirtualPath, IWebFormsCompilationFeature, DependencyParser>> Parsers { get; } = new(StringComparer.OrdinalIgnoreCase);
+
+    public void RegisterPrefix(string tagPrefix, string namespaceName, string assemblyName) => Entries.Add(new(tagPrefix, namespaceName, assemblyName));
 
     internal void AddParser<DParser>(string extension)
         where DParser : DependencyParser, new()
