b9d38df16f6122d885d657c90ad2980027555cae
twsouthwick <twsouthwick@outlook.com>
2024-03-28 14:25:19 -0700

    Add infra project and tag prefix option

---
+++ src/Infrastructure/DynamicModuleUtility.cs
@@ -0,0 +1,8 @@
+﻿using System.Web;
+
+namespace Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+public static class DynamicModuleUtility
+{
+    public static void RegisterModule(Type moduleType) => HttpApplication.RegisterModule(moduleType);
+}
