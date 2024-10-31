c8e051f29cda17d18dcd7363fc8bc419eea0c38e
Mason Chase <mason.chase@quandago.com>
2024-10-31 09:57:48 +0000

    feat: Remove hot-reload feature from dynamic compilation

--- src/Compiler.Dynamic/WebFormsCompilationService.cs
+++ src/Compiler.Dynamic/WebFormsCompilationService.cs
@@ -1,93 +1,30 @@
 // MIT License.
 
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.Extensions.Primitives;
 
 namespace WebForms.Compiler.Dynamic;
 
-internal sealed class WebFormsCompilationService : BackgroundService
+internal sealed class WebFormsCompilationService(
+    DynamicSystemWebCompilation compiler,
+    ILogger<WebFormsCompilationService> logger)
+    : BackgroundService
 {
-    private readonly ILogger<WebFormsCompilationService> _logger;
-    private readonly ManualResetEventSlim _event;
-    private readonly DynamicSystemWebCompilation _compiler;
-    private readonly IOptions<PageCompilationOptions> _options;
-
-    public WebFormsCompilationService(
-        DynamicSystemWebCompilation compiler,
-        IOptions<PageCompilationOptions> options,
-        ILogger<WebFormsCompilationService> logger)
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _compiler = compiler;
-        _options = options;
-        _logger = logger;
-        _event = new ManualResetEventSlim(true);
+        ProcessChanges(stoppingToken);
+        return Task.CompletedTask;
     }
 
-    public override void Dispose()
+    private void ProcessChanges(CancellationToken token)
     {
-        base.Dispose();
-        _event.Dispose();
-    }
-
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        var files = _compiler.Files;
-
-        using var matcher = new CompositeWatcher(files, OnFileChange);
-
-        foreach (var extension in _options.Value.Parsers.Keys)
+        try
         {
-            matcher.AddExtension(extension);
+            compiler.CompilePages(token);
         }
-
-        await ProcessChanges(stoppingToken).ConfigureAwait(false);
-    }
-
-    private void OnFileChange()
-    {
-        _event.Set();
-        _logger.LogInformation("File change detected");
-    }
-
-    private async Task ProcessChanges(CancellationToken token)
-    {
-        while (!token.IsCancellationRequested)
+        catch (Exception ex)
         {
-            await _event.WaitHandle.WaitAsync(token).ConfigureAwait(false);
-
-            try
-            {
-                _compiler.CompilePages(token);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error compiling assets");
-            }
-
-            _event.Reset();
-        }
-    }
-
-    private sealed class CompositeWatcher(IFileProvider files, Action action) : IDisposable
-    {
-        private readonly List<IDisposable> _disposables = [];
-
-        public void AddExtension(string extension)
-        {
-            var disposable = ChangeToken.OnChange(() => files.Watch($"**/*{extension}*"), action);
-
-            _disposables.Add(disposable);
-        }
-
-        public void Dispose()
-        {
-            foreach (var d in _disposables)
-            {
-                d.Dispose();
-            }
+            logger.LogError(ex, "Error compiling assets");
         }
     }
 }
