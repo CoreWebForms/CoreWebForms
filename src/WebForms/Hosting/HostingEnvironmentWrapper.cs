28922a8b1fe0398eed8f3de493db202243382769
Jeff <jeff.nasseri@quandago.com>
2024-11-13 11:54:37 +0000

    Add initial set of utility classes and refactor hosting environment.
    This commit introduces several utility classes to the WebForms project to extend functionality. It also refactors the HostingEnvironmentWrapper to enhance path mapping capabilities.
    
    Key changes:
    1. Added `FileEnumerator`, `SafeBitVector32`, `SecUtil`, `SimpleRecyclingCache`, `SingleObjectCollection`, and `Wildcard` utility classes.
    2. Implemented `AppSettings` class for configuration management with secure defaults.
    3. Refactored `HostingEnvironmentWrapper` to manage virtual path mappings effectively.
    4. Modified `FileUtil` class to include additional directory and file operations for better error handling and validation.
    5. Broadened accessibility of `ITypedWebObjectFactory` by changing it from internal to public to ensure broader usage in web applications.
    6. Improved code comments and organization for better readability and maintainability.

---
+++ src/WebForms/Hosting/HostingEnvironmentWrapper.cs
@@ -0,0 +1,128 @@
+﻿// MIT License.
+
+using System.Web.Util;
+
+namespace System.Web.Hosting;
+
+public static class HostingEnvironmentWrapper
+{
+    private static string MapPathActual(VirtualPath virtualPath, bool permitNull)
+        {
+            string result = null;
+
+            Debug.Assert(virtualPath != null);
+
+            virtualPath.FailIfRelativePath();
+
+            VirtualPath reqpath = virtualPath;
+
+            // if (String.CompareOrdinal(reqpath.VirtualPathString, _appVirtualPath.VirtualPathString) == 0) {
+            //     // for application path don't need to call app host
+            //     Debug.Trace("MapPath", reqpath  +" is the app path");
+            //     result = _appPhysicalPath;
+            // }
+            // else {
+            //     // using (new ProcessImpersonationContext()) {
+            //         // If there is a mapping for this virtual path in the call context, use it
+            //         result = GetVirtualPathToFileMapping(reqpath);
+            //
+            //         if (result == null) {
+            //             // call host's mappath
+            //             if (_configMapPath == null) {
+            //                 Debug.Trace("MapPath", "Missing _configMapPath");
+            //                 throw new InvalidOperationException(SR.GetString(SR.Cannot_map_path, reqpath));
+            //             }
+            //             Debug.Trace("MapPath", "call ConfigMapPath (" + reqpath + ")");
+            //
+            //             // see if the IConfigMapPath provider implements the interface
+            //             // with VirtualPath
+            //             try {
+            //                 if (null != _configMapPath2) {
+            //                     result = _configMapPath2.MapPath(GetSiteID(), reqpath);
+            //                 }
+            //                 else {
+            //                     result = _configMapPath.MapPath(GetSiteID(), reqpath.VirtualPathString);
+            //                 }
+            //                 if (HttpRuntime.IsMapPathRelaxed)
+            //                     result = HttpRuntime.GetRelaxedMapPathResult(result);
+            //             } catch {
+            //                 if (HttpRuntime.IsMapPathRelaxed)
+            //                     result = HttpRuntime.GetRelaxedMapPathResult(null);
+            //                 else
+            //                     throw;
+            //             }
+            //         }
+            //     // }
+            // }
+            //
+            // if (String.IsNullOrEmpty(result)) {
+            //     Debug.Trace("MapPath", "null Result");
+            //     if (!permitNull) {
+            //         if (HttpRuntime.IsMapPathRelaxed)
+            //             result = HttpRuntime.GetRelaxedMapPathResult(null);
+            //         else
+            //             throw new InvalidOperationException(SR.GetString(SR.Cannot_map_path, reqpath));
+            //     }
+            // }
+            // else {
+            //     // ensure extra '\\' in the physical path if the virtual path had extra '/'
+            //     // and the other way -- no extra '\\' in physical if virtual didn't have it.
+            //     if (virtualPath.HasTrailingSlash) {
+            //         if (!UrlPath.PathEndsWithExtraSlash(result) && !UrlPath.PathIsDriveRoot(result))
+            //             result = result + "\\";
+            //     }
+            //     else {
+            //         if (UrlPath.PathEndsWithExtraSlash(result) && !UrlPath.PathIsDriveRoot(result))
+            //             result = result.Substring(0, result.Length - 1);
+            //     }
+            //
+            //     Debug.Write("MapPath", "    result=" + result);
+            // }
+
+            return result;
+        }
+
+    internal static string MapPath(VirtualPath virtualPath) {
+
+        String path = MapPathInternal(virtualPath);
+
+        // if (path != null)
+        //     InternalSecurityPermissions.PathDiscovery(path).Demand();
+
+        return path;
+    }
+
+    internal static String MapPathInternal(string virtualPath) {
+        return MapPathInternal(VirtualPath.Create(virtualPath));
+    }
+
+    internal static String MapPathInternal(VirtualPath virtualPath) {
+        return HostingEnvironmentWrapper.MapPathActual(virtualPath, false);
+    }
+
+    internal static String MapPathInternal(string virtualPath, bool permitNull) {
+        return MapPathInternal(VirtualPath.Create(virtualPath), permitNull);
+    }
+
+    internal static String MapPathInternal(VirtualPath virtualPath, bool permitNull) {
+        return HostingEnvironmentWrapper.MapPathActual(virtualPath, permitNull);
+    }
+
+    internal static string MapPathInternal(string virtualPath, string baseVirtualDir, bool allowCrossAppMapping) {
+        return MapPathInternal(VirtualPath.Create(virtualPath),
+            VirtualPath.CreateNonRelative(baseVirtualDir), allowCrossAppMapping);
+    }
+
+    internal static string MapPathInternal(VirtualPath virtualPath, VirtualPath baseVirtualDir, bool allowCrossAppMapping) {
+        Debug.Assert(baseVirtualDir != null, "baseVirtualDir != null");
+
+        // Combine it with the base and reduce
+        virtualPath = baseVirtualDir.Combine(virtualPath);
+
+        // TODO: Migration
+        // if (!allowCrossAppMapping && !virtualPath.IsWithinAppRoot)
+        //     throw new ArgumentException(SR.GetString(SR.Cross_app_not_allowed, virtualPath));
+
+        return MapPathInternal(virtualPath);
+    }
+}
