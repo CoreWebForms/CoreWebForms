dd94ef68c959e526ed769900636e05f09468448e
Jeff <jeff.nasseri@quandago.com>
2024-11-13 11:56:05 +0000

    Add new CachedPathData implementation and extensions
    Added a new `CachedPathData` class to provide caching behavior for path data within the web application framework. Introduced multiple new extension files within WebForms, including `HttpContextServerVariableExtensions` and `HttpServerUtilityExtensions`, for handling server variables and HTML encoding utilities. Adjusted project file (`WebForms.csproj`) to include new dependencies and folder structure.
    
    Key changes:
    1. Implemented `CachedPathData` class in `CachedPathData.cs` for managing cached path data with methods for initialization and removal.
    2. Created `HttpContextServerVariableExtensions.cs` to offer server variable extraction functionality.
    3. Added `HttpServerUtilityExtensions.cs` for HTML encode/decode methods.
    4. Updated `MultiTargetingUtil` with framework resolution logic relevant for Client Build Manager scenarios.
    5. Modified `CompiledReflectionWebFormsPage.cs` by renaming parameter to `_next` for clarity.
    6. Updated `WebForms.csproj` to incorporate new folders and additional package references, ensuring proper project structuring and dependency management.
    7. Update SR.cs.

---
+++ src/WebForms/HttpServerUtilityExtensions.cs
@@ -0,0 +1,12 @@
+﻿// MIT License.
+
+namespace System.Web;
+
+public static class HttpServerUtilityExtensions
+{
+    public static string HtmlEncode(this HttpServerUtility server, string s)
+        => HttpUtility.HtmlEncode(s);
+
+    public static string HtmlDecode(this HttpServerUtility server, string s)
+        => HttpUtility.HtmlDecode(s);
+}
