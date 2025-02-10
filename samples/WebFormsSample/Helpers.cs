8e490e1aa951f3ceac98498c0f1582e1a5d80201
birojkumarnayak <birojkumarnayak@gmail.com>
2025-02-09 20:00:31 -0800

    Loading referenced assemblies to current app domain

--- samples/WebFormsSample/Helpers.cs
+++ samples/WebFormsSample/Helpers.cs
@@ -1,8 +1,29 @@
 // MIT License.
 
+using System.Data.Entity;
+
 namespace WebFormsSample;
 
+//
 public class Helpers
 {
     public static string Message = "Hello from a helper";
 }
+
+public class SampleContext : DbContext
+{
+    public SampleContext()
+    {
+        Database.SetInitializer<SampleContext>(null);
+    }
+
+    public DbSet<Author>? Authors { get; set; }
+}
+
+public class Author
+{
+    public int AuthorId { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
+
