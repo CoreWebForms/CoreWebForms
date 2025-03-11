e8958c45722fa040017b660f7d22c8996c3bab61
Taylor Southwick <tasou@microsoft.com>
2025-03-11 16:55:09 -0700

    Consolidate IHttpHandler endpoint creation to ensure delegate is set
    The RequestDelegate was not being set for one of the pathways that creates endpoints from IHttpHandler. This change moves all the creation of Endpoint/EndpointBuilder into IHttpHandlerEndpointFactory so that it is in one place and will be set the same for all endpoints.

--- src/Handlers/IHttpHandlerEndpointFactory.cs
+++ src/Handlers/IHttpHandlerEndpointFactory.cs
@@ -1,11 +1,15 @@
 // MIT License.
 
 using System.Web;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing.Patterns;
 
 namespace Microsoft.AspNetCore.SystemWebAdapters.HttpHandlers;
 
 internal interface IHttpHandlerEndpointFactory
 {
     Endpoint Create(IHttpHandler handler);
+
+    EndpointBuilder CreateBuilder(RoutePattern pattern);
 }
