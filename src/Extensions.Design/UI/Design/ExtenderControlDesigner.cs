59c369e23af9fa34738c8e731c5ddbeb42681bdb
birojkumarnayak <biroj@amazon.com>
2025-03-14 17:34:30 -0700

    ControlDesigner commit

--- src/Extensions.Design/UI/Design/ExtenderControlDesigner.cs
+++ src/Extensions.Design/UI/Design/ExtenderControlDesigner.cs
@@ -2,18 +2,38 @@
 
 using System.Collections;
 using System.ComponentModel;
-using System.Runtime;
 
 namespace System.Web.UI.Design;
 
 public class ExtenderControlDesigner : ControlDesigner, IControlDesigner
 {
-    public ExtenderControlDesigner();
+    public ExtenderControlDesigner() { }
 
     protected override bool Visible { get; }
 
-    public override string GetDesignTimeHtml();
-    public override void Initialize(IComponent component);
-    protected override void Dispose(bool disposing);
-    protected override void PreFilterProperties(IDictionary properties);
+    bool IControlDesigner.Visible => throw new NotImplementedException();
+
+    public override string GetDesignTimeHtml() => throw new NotImplementedException();
+    public override void Initialize(IComponent component) => throw new NotImplementedException();
+
+    public void UpdateDesignTimeHtml()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected  void Dispose(bool disposing) => throw new NotImplementedException();
+    protected override void PreFilterProperties(IDictionary properties) => throw new NotImplementedException();
+
+    string IControlDesigner.CreatePlaceHolderDesignTimeHtml()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class ControlDesigner 
+{
+    protected virtual bool Visible { get; }
+    public virtual string GetDesignTimeHtml() => throw new NotImplementedException();
+    public virtual void Initialize(IComponent component) => throw new NotImplementedException();
+    protected virtual void PreFilterProperties(IDictionary properties) => throw new NotImplementedException();
 }
